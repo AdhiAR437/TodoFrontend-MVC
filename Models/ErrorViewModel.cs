da6dff758f665c7b6905e35c34788e7e3008f386
AdhiAR437 <adhiar98@gmail.com>
2024-10-16 15:38:57 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TodoFrontend_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
